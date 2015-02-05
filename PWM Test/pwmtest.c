616dcc298fe0d8ff99b3cc60b4ceb971eb852905
nick <nick.lapin@gmail.com>
2015-02-04 23:16:23 -0500

    motor pwm

--- PWM Test/pwmtest.c
+++ PWM Test/pwmtest.c
@@ -11,19 +11,29 @@ int16_t main(void) {
 	init_ui();
 	init_oc();
 	init_timer();	
+	// Pin 2 - D2
 	pin_digitalOut(&D[2]);
+	// Pin 3 - D1
 	pin_digitalOut(&D[3]);
+	// Pin 4 - ENA
 	pin_digitalOut(&D[4]);
+	// Pin 5 - IN2
 	pin_digitalOut(&D[5]);
+	// Pin 6 - IN1
 	pin_digitalOut(&D[6]);
+
+
+
 
         while (1) {
 			led_on(&led1);
 
 			//High
-			pin_set(&D[2]);
-			pin_set(&D[4]);
-			pin_set(&D[5]);
+
+			pin_set(&D[5]); // In2 is high
+			pin_set(&D[4]); // Ena
+			oc_pwm(&oc5,&D[2],&timer3,5,100);
+			//pin_set(&D[5]);
 
 			//Low
 			pin_clear(&D[3]);
